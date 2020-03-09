b211cae8070530c111c9471f7c5423de7976e84c
Nick Tustison <ntustison@gmail.com>
2020-03-09 10:16:57 -0700

    ENH:  Add multiple marathons plan--6 week.

--- makePfitzingerIcal.R
+++ makePfitzingerIcal.R
@@ -2,8 +2,9 @@ library( lubridate )
 
 #############
 
-marathonGoalDate <- ymd( 20200308 )
-plan <- read.csv( "PfitzingerPlans/Pfitzinger_70to85_12week.csv", header = TRUE )
+marathonGoalDate <- ymd( 20200426 )
+# plan <- read.csv( "PfitzingerPlans/Pfitzinger_70to85_12week.csv", header = TRUE )
+plan <- read.csv( "PfitzingerPlans/Pfitzinger_MultipleMarathons_6week.csv", header = TRUE )
 outputFile <- "plan.ics"
 
 ##############
